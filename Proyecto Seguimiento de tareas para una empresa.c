f763e9411a84427d5c7fee80302b0e1d466c15b1
santty1906 <santiagodlo.19@gmail.com>
2024-11-16 13:56:41 -0500

    Update Proyecto Seguimiento de tareas para una empresa.c

--- Proyecto Seguimiento de tareas para una empresa.c
+++ Proyecto Seguimiento de tareas para una empresa.c
@@ -3,7 +3,59 @@
 
 
 
-
 int main (){
+    char 
+    int opcion = 0, i = 0;
+    float
+
+    do{
+
+        printf("Menú de opciones: (Ingrese un número como opción)\n");
+        printf("1. Ingresar datos de las tareas.\n");
+        printf("2. Ingresar estado semanal de cada tarea.\n");
+        printf("3. Mostrar total de tareas completadas por proyecto.\n");
+        printf("4. Mostrar proyecto con mayor y menor porcentaje de tareas completadas.\n");
+        printf("5. Mostrar porcentaje de progreso de cada proyecto.\n");
+        printf("6. Salir.\n");
+        printf("\nSeleccione una opción: ");
+        scanf("%d", &opcion);
+        
+        if (opcion < 1 || opcion > 6){
+            printf("Error al escoger la opción, por favor verifique e introduzca el número nuevamente");
+        
+        }else{
+            switch(opcion){
+                case 1:{
+                    
+                    break;
+                }
+
+                case 2:{
+
+                    break;
+                }
+
+                case 3:{
+
+                    break;
+                }
+
+                case 4:{
+
+                    break;
+                }
+
+                case 5:{
+
+                    break;
+                }
+            }
+
+
+        }
     
+    
+    }while( opcion != 6);
+    
+    return 0;
 }
